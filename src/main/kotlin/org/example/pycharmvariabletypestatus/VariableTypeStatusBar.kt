442689913e00599d5804266cff4df9552010591f
abanoubseddik <abanoub.maged171@gmail.com>
2025-03-17 14:05:03 +0100

    Add status bar widget factory

---
+++ src/main/kotlin/org/example/pycharmvariabletypestatus/VariableTypeStatusBar.kt
@@ -0,0 +1,4 @@
+package org.example.pycharmvariabletypestatus
+
+class VariableTypeStatusBar {
+}
---
+++ src/main/kotlin/org/example/pycharmvariabletypestatus/VariableTypeStatusBarFactory.kt
@@ -0,0 +1,17 @@
+package org.example.pycharmvariabletypestatus
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.StatusBar
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+
+class VariableTypeStatusBarFactory : StatusBarWidgetFactory {
+    override fun getId(): String = "VariableTypeStatusBar"
+    override fun getDisplayName(): String = "Variable Type Display"
+    override fun isAvailable(project: Project): Boolean = true
+    override fun createWidget(project: Project): StatusBarWidget = VariableTypeStatusBar(project)
+    override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
+    override fun disposeWidget(widget: StatusBarWidget) {
+        widget.dispose()
+    }
+}
